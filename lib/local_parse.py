84eeb6e71cdc5a1508f8377ba5fc5dbfc09f7b6d
Chris Lanus <cqlanus@gmail.com>
2020-07-23 10:54:26 -0500

    updates to pdf parsing

--- lib/local_parse.py
+++ lib/local_parse.py
@@ -4,7 +4,7 @@ from pdfminer.high_level import extract_text
 def main():
     """testing
     """
-    pdf = './lib/data/cs_mofi.pdf'
+    pdf = './lib/data/pg_scsc.pdf'
     filepath = './lib/data/sampleText.txt'
     text = extract_text(pdf)
     with open(filepath, 'wt') as f:
